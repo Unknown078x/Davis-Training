d7c827d9d74654f1572ff5c87fed864b4fa0f25f
Unknown078x <prajapati.shiva.125@gmail.com>
2026-03-06 11:23:31 +0530

    06-03-2026

---
+++ Classwork/circleOperation.py
@@ -0,0 +1,9 @@
+radius=5
+
+#Calculating Area of circle
+area=3.14*radius*radius
+print("Area of Circle: ",area)
+
+#Calculating Perimeter of circle
+parimeter=2*3.14*radius
+print("Perimeter of Circle: ",parimeter)
