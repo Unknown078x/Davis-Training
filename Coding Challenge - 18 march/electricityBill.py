4fcf3532788e729893896697e4c3b06b9d41e591
Unknown078x <prajapati.shiva.125@gmail.com>
2026-03-18 09:41:44 +0530

    question-3 electricity bill

---
+++ Coding Challenge - 18 march/electricityBill.py
@@ -0,0 +1,8 @@
+#taking input of units consumed by the user
+units=int(input("Enter the units consumed:"))
+
+# let the price of one unit be 8 rupees
+bill_amount=8*units
+
+#printing the bill amount to user
+print("Total bill amount: Rs.",bill_amount)
