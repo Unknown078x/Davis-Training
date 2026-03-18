d866a9e070f0bad58521bf7e5a2a5e99ccda3daa
Unknown078x <prajapati.shiva.125@gmail.com>
2026-03-18 10:52:01 +0530

    question-18 electricity bill logic-4

--- Coding Challenge - 18 march/electricityBill4.py
+++ Coding Challenge - 18 march/electricityBill4.py
@@ -1,3 +1,9 @@
-#using lambda function
-calc = lambda units: units * 8
-print("Bill:", calc(int(input("Enter units: "))))
+#function to calculate the bill
+def calculateBill(units):
+   print("Total bill: Rs.",8*units)
+
+#taking input of units consumed by the user
+units=int(input("Enter the units consumed:"))
+
+# calling the function to print the electricity bill
+calculateBill(units)
---
+++ Coding Challenge - 18 march/electricityBill5.py
@@ -0,0 +1,3 @@
+# using lambda function
+calc = lambda u: u * 8
+print("Bill:", calc(int(input("Enter units: "))))
