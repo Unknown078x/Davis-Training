cee2488fbdcd6fd2f3b47b2727f3108c814983a5
Unknown078x <prajapati.shiva.125@gmail.com>
2026-03-18 10:43:26 +0530

    question-21 bill after discount logic-5

---
+++ Coding Challenge - 18 march/billAfterDiscount5.py
@@ -0,0 +1,13 @@
+# function to calculate the bill after discount
+def calculateFinalBill(price,discount):
+    final_price=price -  (discount / 100) * price
+    return final_price
+
+#taking the price from user
+price = int(input("Enter price: "))
+
+#taking the discount percentage from the user
+discount = int(input("Enter discount percentage: "))
+
+# calling the function to calculate the bill
+print("Final bill after discount: Rs.",calculateFinalBill(price,discount))
