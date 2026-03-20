e064bad04b7196895798f0992c5bd14c7769d2d3
Unknown078x <prajapati.shiva.125@gmail.com>
2026-03-20 10:10:12 +0530

    sorting elements in descending order

--- Classwork/sortingElements.py
+++ Classwork/sortingElements.py
@@ -1,18 +1,18 @@
-numbers = [12, 5, 8, 23, 1, 45, 9, 3, 17, 6]
+numbers = [20,19,222,56,33,2,0,21,67,53]
 
 # loop to iterate through the list
 for i in range(len(numbers)):
     # assuming the first element to be the biggest element and saving its index
-    max = i
+    maxElement = i
     
     # second loop to iterate through the list to compare the other elements with the first element
     for j in range(i+1, len(numbers)):
-        # checking if the next number is greater than the assumed max number, if true replace the index
-        if numbers[j] > numbers[max]:
-            max = j
+        # checking if the next element is greater than the assumed max element, if true replace the index
+        if numbers[j] > numbers[maxElement]:
+            maxElement = j
     
-    # after the iterations of second loop, replacing the highest number to the first position and so on
-    numbers[i], numbers[max] = numbers[max], numbers[i]
+    # after the iterations of second loop, replacing the highest element to the first position and so on
+    numbers[i], numbers[maxElement] = numbers[maxElement], numbers[i]
 
 # printing the elements in descending order
 print("Sorted list in descending order:", numbers)
