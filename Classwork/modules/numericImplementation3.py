46e6db7c16da2450b4e590473c8ef365228250b5
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-08 10:43:10 +0530

    importing specific functions from the module

---
+++ Classwork/modules/numericImplementation3.py
@@ -0,0 +1,9 @@
+from numericCalculation import calculateSum,calculateProduct
+
+a=20
+b=5
+
+
+# calling functions from the numeric calculation module
+print(f"Sum of {a} and {b} is {calculateSum(a,b)}")
+print(f"Product of {a} and {b} is {calculateProduct(a,b)}")
