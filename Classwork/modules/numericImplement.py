661c3306cde466670c340443dce6069cd75c77b6
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-08 10:25:02 +0530

    Modules

---
+++ Classwork/modules/numericImplement.py
@@ -0,0 +1,12 @@
+import numericCalculation
+
+a=10
+b=20
+
+# calling functions from the numeric calculation module
+print(f"Sum of {a} and {b} is {numericCalculation.calculateSum(a,b)}")
+print(f"Difference of {a} and {b} is {numericCalculation.calculateDifference(a,b)}")
+print(f"Product of {a} and {b} is {numericCalculation.calculateProduct(a,b)}")
+print(f"Division of {a} and {b} is {numericCalculation.calculateDivision(a,b)}")
+print(f"Modulus of {a} and {b} is {numericCalculation.calculateModulus(a,b)}")
+
---
+++ Classwork/modules/numericImplementation2.py
@@ -0,0 +1,12 @@
+from numericCalculation import *
+
+a=20
+b=5
+
+
+# calling functions from the numeric calculation module
+print(f"Sum of {a} and {b} is {calculateSum(a,b)}")
+print(f"Difference of {a} and {b} is {calculateDifference(a,b)}")
+print(f"Product of {a} and {b} is {calculateProduct(a,b)}")
+print(f"Division of {a} and {b} is {calculateDivision(a,b)}")
+print(f"Modulus of {a} and {b} is {calculateModulus(a,b)}")
