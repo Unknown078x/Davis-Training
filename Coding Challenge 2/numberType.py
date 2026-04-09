a86a30137af04323bf3e963cb1b453222a162941
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-09 09:32:09 +0530

    program to check if a number is positive, negative, or zero.

---
+++ Coding Challenge 2/numberType.py
@@ -0,0 +1,10 @@
+# taking number from user 
+n = int(input("Enter a number:"))
+
+# checking the type of number and printing it
+if n > 0:
+    print("Positive")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
