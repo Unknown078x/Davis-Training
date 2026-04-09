5641e0a1ea77178c0e48a4301d9438018485eb84
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-09 10:20:48 +0530

    check if key exists in dictionary.

---
+++ Coding Challenge 2/checkKey.py
@@ -0,0 +1,10 @@
+# dictionary
+data = {"a":1,"b":2}
+# taking key as input from user
+key = input("Enter key: ")
+
+# checking if the key exists in dictionary or not
+if key in data:
+    print("Yes")
+else:
+    print("No")
