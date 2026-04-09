31b37d7e769f85669ba1554c1c3bce4b4ca43490
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-09 09:35:15 +0530

    program to calculate total weekly sales.

---
+++ Coding Challenge 2/weeklySales.py
@@ -0,0 +1,10 @@
+# let the sales in a list
+sales = [100,200,150,300,250,400,100]
+
+# taking a total variable to store the total sale
+total = 0
+# loop to iterate through the sales list to sum the total sales
+for s in sales:
+    total += s
+
+print(total)
