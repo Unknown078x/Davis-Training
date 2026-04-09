405e6b29ac506adefa6d661bee98269de590e8a3
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-09 10:25:06 +0530

    add two numbers by importing a module

---
+++ Coding Challenge 2/myModule.py
@@ -0,0 +1,19 @@
+# function to add two numbers 
+def calculateSum(a,b):
+    return a+b
+
+#function to subtract two numbers
+def calculateDifference(a,b):
+    return a-b
+
+#function to multiply two numbers
+def calculateProduct(a,b):
+    return a*b
+
+#function to divide two numbers
+def calculateDivision(a,b):
+    return a/b
+
+#function to find modulus of two numbers
+def calculateModulus(a,b):
+    return a%b
---
+++ Coding Challenge 2/performCalculation.py
@@ -0,0 +1,4 @@
+import myModule
+
+
+print(myModule.add(2,3))
