2704b32549f0e73eef0c57335f3cd8a47ed5ae2e
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-09 10:29:16 +0530

    check prime number.

---
+++ Coding Challenge 2/checkPrime.py
@@ -0,0 +1,12 @@
+# function to check if a number is prime or not
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+
+    return True
+
+print("Prime" if is_prime(7) else "Not Prime")
