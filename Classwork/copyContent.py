7dedb555665d17c0b4384b2905b7c01bf6b31833
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-16 09:43:15 +0530

    copy content to new file

--- Classwork/copyContent.py
+++ Classwork/copyContent.py
@@ -1,18 +1,18 @@
 try:
     # opening the file in read mode
-    with open("source.txt", "r") as file:
+    with open("Classwork/hcf.py", "r") as file:
         # reading the content of file
         content = file.read()
 
     # creating new file and writing data into it
-    with open("destination.txt", "w") as file:
+    with open("Classwork/destination.txt", "w") as file:
         # writing the data into the file
         file.write(content)
 
     print("File copied successfully.")
 
 except FileNotFoundError:
-    print("Error: source.txt file does not exist.")
+    print("Error: file does not exist.")
 
 except IOError:
     print("Error: Problem while opening or reading the file.")
