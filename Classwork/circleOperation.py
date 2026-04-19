06aea06405f1cdaa8bc41f1532dbfc881ef0ef13
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-19 12:17:34 +0530

    Removed unnecessary files

--- Classwork/circleOperation.py
+++
@@ -1,9 +0,0 @@
-radius=5
-
-#Calculating Area of circle
-area=3.14*radius*radius
-print("Area of Circle: ",area)
-
-#Calculating Perimeter of circle
-perimeter=2*3.14*radius
-print("Perimeter of Circle: ",perimeter)
