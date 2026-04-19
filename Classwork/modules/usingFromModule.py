40ae75be8b708828cfa5de3d154f21db991ff026
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-19 09:47:39 +0530

    module program

---
+++ Classwork/modules/usingFromModule.py
@@ -0,0 +1,19 @@
+from Classwork.modules.numericCalculations import *
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+#Adding two numbers by calling the function from numericCalculations module
+print(f"sum of {a} and {b} is: ",addTwoNumbers(a,b))
+
+#Subtracting two numbers by calling the function from numericCalculations module
+print(f"Difference of {a} and {b} is: ",subtractTwoNumbers(a,b))
+
+#Multiplying two numbers by calling the function from numericCalculations module
+print(f"Multiplication of {a} and {b} is: ",multiplyTwoNumbers(a,b))
+
+#Dividing two numbers by calling the function from numericCalculations module
+print(f"Division of {a} and{b} is: ",divideTwoNumbers(a,b))
+
+#Modulus of two numbers by calling the function from numericCalculations module
+print(f"Modulus of {a} and {b} is: ",modulusTwoNumbers(a,b))
---
+++ Classwork/modules/usingModule.py
@@ -0,0 +1,19 @@
+import Classwork.modules.numericCalculations as mod
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+#Adding two numbers by calling the function from numericCalculations module
+print(f"sum of {a} and {b} is: ",mod.addTwoNumbers(a,b))
+
+#Subtracting two numbers by calling the function from numericCalculations module
+print(f"Difference of {a} and {b} is: ",mod.subtractTwoNumbers(a,b))
+
+#Multiplying two numbers by calling the function from numericCalculations module
+print(f"Multiplication of {a} and {b} is: ",mod.multiplyTwoNumbers(a,b))
+
+#Dividing two numbers by calling the function from numericCalculations module
+print(f"Division of {a} and{b} is: ",mod.divideTwoNumbers(a,b))
+
+#Modulus of two numbers by calling the function from numericCalculations module
+print(f"Modulus of {a} and {b} is: ",mod.modulusTwoNumbers(a,b))
