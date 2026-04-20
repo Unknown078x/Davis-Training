e173b4b94e700d48779df105e646e23916beeca0
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-20 10:01:32 +0530

    Creating array using numpy with different and same data types

---
+++ Classwork/NUMPY/createArray.py
@@ -0,0 +1,10 @@
+#to create array with int type data only 
+import numpy as np
+
+# it will give an array with int data type elements
+numbers = np.array([20,30,40,70,90])
+print(numbers)
+
+
+# output
+# [20 30 40 70 90]
---
+++ Classwork/NUMPY/createArray3.py
@@ -0,0 +1,8 @@
+# creating array with string, float and int type data
+import numpy as np
+# it will give an array of type string converting all the other elements in string
+data = np.array([45,"python",6.7])
+print(data)
+
+# output
+# ['45' 'python' '6.7']
