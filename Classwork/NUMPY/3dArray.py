16f7a5e09a3d7e59fce11f0c55ec79d91934b1bc
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-20 10:23:50 +0530

    3d array using zeros() function

---
+++ Classwork/NUMPY/3dArray.py
@@ -0,0 +1,28 @@
+# create a 3d array of order 3,6,2 of int data type
+import numpy as np
+
+array4 = np.zeros((3,6,2) , dtype='int')
+print(array4)
+
+
+# output
+# [[[0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]]
+
+#  [[0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]]
+
+#  [[0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]
+#   [0 0]]]
