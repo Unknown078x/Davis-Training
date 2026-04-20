efa9b018cf477109a8f17df482b1f7fa366024b6
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-20 10:13:51 +0530

    using arange() function

---
+++ Classwork/NUMPY/usingArange.py
@@ -0,0 +1,10 @@
+# program to iterate through a specified sequence using arange() function with step count
+import numpy as np
+
+# it will work as a loop that starts from 10 to 100 with the step of 5
+x = np.arange(10,100,5)
+print(x)
+
+
+# output
+#[10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95]
