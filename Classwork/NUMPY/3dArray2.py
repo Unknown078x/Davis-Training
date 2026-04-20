aca011f701b07848808c96f44e2ca401aa647c41
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-20 10:27:28 +0530

    creating array using ones() function

---
+++ Classwork/NUMPY/3dArray2.py
@@ -0,0 +1,18 @@
+# create a 3d array of order 2,5,1 of int data type
+import numpy as np
+
+array4 = np.ones((2,5,1) , dtype='int')
+print(array4)
+
+# output
+# [[[1]
+#   [1]
+#   [1]
+#   [1]
+#   [1]]
+
+#  [[1]
+#   [1]
+#   [1]
+#   [1]
+#   [1]]]
---
+++ Classwork/NUMPY/usingOnes.py
@@ -0,0 +1,10 @@
+# create a 2d array of specified order of elements 1s with float data type by default
+import numpy as np
+
+array3 = np.ones((3,3))
+print(array3)
+
+# output
+# [[1. 1. 1.]
+#  [1. 1. 1.]
+#  [1. 1. 1.]]
