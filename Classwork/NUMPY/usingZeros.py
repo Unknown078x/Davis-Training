37681f619084a29b0752318573b60c3f3f09d9c1
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-20 10:21:23 +0530

    creating 2d array with zeros() function

---
+++ Classwork/NUMPY/usingZeros.py
@@ -0,0 +1,25 @@
+#to create 2d array using zeros function with column and rows of specified order
+import numpy as np
+
+# it will create a 2d array of float data type with zero values in it
+array3 = np.zeros((3,3))
+print(array3)
+
+
+# output
+# [[0. 0. 0.]
+#  [0. 0. 0.]
+#  [0. 0. 0.]]
+
+
+
+# it will create a 2d array of int data type
+array4 = np.zeros((4,5) , dtype='int')
+print(array4)
+
+# output
+# [[0 0 0 0 0]
+#  [0 0 0 0 0]
+#  [0 0 0 0 0]
+#  [0 0 0 0 0]]
+
