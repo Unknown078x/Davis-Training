53fb0e6fd42da9779db6aa9e23bcd1ca6b6124d2
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-23 11:02:27 +0530

    calculating Mean and median of array using numpy

---
+++ Classwork/NUMPY/meanMedian.py
@@ -0,0 +1,25 @@
+# finding the Mean and Median of the array elements
+import numpy as np
+
+# creating array
+arr=np.array([10,40,20,50,22,12,35,78])
+
+# printing the array
+print("Array:\n",arr)
+
+# finding the mean value using the mean method
+meanValue=np.mean(arr)
+
+# printing the mean of array
+print("Mean of the Array:",meanValue)
+
+# finding the median of array using the median method
+medianValue=np.median(arr)
+
+# printing the median of array
+print("Median of Array:",medianValue)
+
+
+
+
+
