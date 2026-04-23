4bd82a2ea0d2c48609ce51578c54822f257cd13c
Unknown078x <prajapati.shiva.125@gmail.com>
2026-04-23 11:11:30 +0530

    finding standard deviation

---
+++ Classwork/NUMPY/standardDeviation.py
@@ -0,0 +1,15 @@
+# finding the Standard Deviation of the array elements
+import numpy as np
+
+# creating array
+arr=np.array([10,40,30,50])
+
+# printing the array
+print("Array:\n",arr)
+
+# finding the value of standard deviation
+deviationValue=np.std(arr)
+
+# printing the value of standard deviation
+print("Standard Deviation of Array:",deviationValue)
+
